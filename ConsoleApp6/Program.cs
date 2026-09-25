c3343973ae402f9cc0ac5c8413ef48c2bd23473a
243567 <243567@KB37-118-C08>
2026-09-25 09:54:31 +0300

    Начали работу

--- ConsoleApp6/Program.cs
+++ ConsoleApp6/Program.cs
@@ -10,6 +10,28 @@ namespace ConsoleApp6
     {
         static void Main(string[] args)
         {
+            List<int> historyWords = new List<int>();
+            List<string> historyShortest = new List<string>();
+            List<int> historyCountSentences = new List<int>();
+            List<int> historyCountVowels = new List<int>();
+            List<int> historyCountConsonantst = new List<int>();
+            List<string> historyLongest = new List<string>();
+
+            bool continueWork = true;
+
+            while (continueWork)
+            {
+                string text;
+
+                while (true)
+                {
+                    Console.WriteLine("Введите текст минимум 100 символов");
+                    text = Console.ReadLine();
+
+
+                }
+            }
+
         }
     }
 }
