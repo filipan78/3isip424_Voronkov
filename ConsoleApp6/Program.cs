9dee78de2c32623c2e00558eb57dca8bc3186917
243567 <243567@KB37-118-C08>
2026-09-25 10:29:16 +0300

    Написали подсчет слов, предложений и гласных/согласных

--- ConsoleApp6/Program.cs
+++ ConsoleApp6/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -28,9 +29,81 @@ namespace ConsoleApp6
                     Console.WriteLine("Введите текст минимум 100 символов");
                     text = Console.ReadLine();
 
+                    if (text.Length >= 100) 
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Текст слишком котороткий");
+                    }
+                }
+                List<string> words = GetWords(text);
+
+                int wordCount = words.Count;
+                
+                string shortest = words[0];
+                string longest = words[0];
+
+                for (int i = 1; i < wordCount; i++)
+                {
+                    if (words[i].Length == shortest.Length)
+                    {
+                        shortest = words[i];
+                    }
+                    if (words[i].Length == longest.Length)
+                    {
+                        longest = words[i];
+                    }
+                }
+                int sentenceCoint = 0;
+
+                for (int i = 0; i < wordCount; i++)
+                {
+                    char c = text[i];
+
+                    if (c == '!' || c == '?' || c == '.')
+                    {
+                        sentenceCoint++;
+                    }
+                }
+                char[] vowels = { 'а', 'е', 'ё', 'и', 'о', 'у', 'ы', 'э', 'ю', 'я' };
+
+                int vowelsCount = 0;
+                int consonantCount = 0;
+
+                List<char> letters = new List<char>();
+                List<int> count = new List<int>();
+
+                for (int i = 0; i < wordCount;i++) {
+                    char c = char.ToLower(text[i]);
+
+                    if (char.IsLetter(c))
+                    {
+                        bool isVowel = false;
+
+                        for (int j = 0; j < vowels.Length; j++)
+                        {
+                            if (c == vowels[j])
+                            {
+                                isVowel = true;
+                            }
+                        }
+                            if (isVowel)
+                            {
+                                vowelsCount++;
+                            }
+                            else
+                            {
+                                consonantCount++;
+                            }
+
+                        
+
+
+
 
                 }
-            }
 
         }
     }
